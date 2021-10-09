0e4b95c5c6ea1508481c0989576f9b435b6dc04e
henjue <henjue@gmail.com>
2021-10-09 11:52:25 +0800

    update

--- resources/user_test.go
+++ resources/user_test.go
@@ -2,6 +2,7 @@ package resources
 
 import (
 	"encoding/base32"
+	"fmt"
 	"testing"
 	"time"
 
@@ -10,12 +11,13 @@ import (
 )
 
 func TestGenerateOTPCode(t *testing.T) {
-	secret := base32.StdEncoding.EncodeToString([]byte("JBSWY3DPEHPK3PXP"))
+	secret := base32.StdEncoding.EncodeToString([]byte("12345678901234567890"))
+	fmt.Println(secret)
 	passcode, err := totp.GenerateCodeCustom(secret, time.Now(), totp.ValidateOpts{
 		Period:    30,
 		Skew:      1,
 		Digits:    otp.DigitsSix,
-		Algorithm: otp.AlgorithmSHA512,
+		Algorithm: otp.AlgorithmSHA1,
 	})
 	if err != nil {
 		panic(err)
