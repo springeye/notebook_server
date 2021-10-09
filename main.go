c8991f1f23dfc1cbe17ec64127a0751f0a3e5856
henjue <henjue@gmail.com>
2021-10-09 13:08:51 +0800

    update

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	db := db2.Database
 	r := gin.Default()
+	r.Static("/admin", "./static/admin")
 	user := resources.UserResource{Db: db}
 	r.GET("/user/login", user.Login)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
